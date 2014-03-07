6c2386bca0c52d03e98fd2a5b00ea0a870833e25
U-MMCOPSE\AntonioT <AntonioT@MM525.copse.mmforest.com>
2014-03-07 16:26:52 +0000

    tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using Nancy.Testing;
+using Nancy.ViewEngines.Razor;
+using NancyApplication1;
+using Xunit;
+
+namespace UnitTestProject1
+{    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Given            
+            var browser = new Browser(with => with.Module<IndexModule>());
+
+            // When
+            var response = browser.Get("/");
+
+            // Then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
